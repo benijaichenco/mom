116dd6aa6510832b25e768cbff0ced17e9a84924
Benjamin <benjaminjaichenco@gmail.com>
2024-02-21 13:11:19 +0200

    change sidenav style, lint script code, add overlay

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const navBtn = document.querySelector(".nav-btn");
 const navMenu = document.querySelector(".nav-menu");
+const overlay = document.querySelector(".overlay");
+const navProjectsTitle = document.querySelector(".projects .link-title");
+const projectsNav = document.querySelector(".projects-nav");
 
-navBtn.addEventListener("click", (e) => {
+function toggleNav() {
   navBtn.classList.toggle("active");
   navMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+function toggleProjectsNav() {
+  navProjectsTitle.classList.toggle("active");
+  projectsNav.classList.toggle("active");
+}
+
+navBtn.addEventListener("click", toggleNav);
+
+overlay.addEventListener("click", function () {
+  if (navBtn.classList.contains("active")) {
+    toggleNav();
+  }
 });
 
 const projectsBtn = document.querySelector(".nav-link.projects .link-title");
-projectsBtn.addEventListener("click", (e) => {
-  document
-    .querySelector(".nav-link.projects .link-title")
-    .classList.toggle("active");
-
-  document
-    .querySelector(".nav-link.projects .projects-nav")
-    .classList.toggle("active");
-});
+projectsBtn.addEventListener("click", toggleProjectsNav);
